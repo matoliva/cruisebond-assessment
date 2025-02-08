6bc65a421aac2f897f687766c6123ca892359787
matoliva <olivamati@gmail.com>
2025-02-09 00:39:59 +1300

    fix: add placeholders when not image

--- components/cruisebound/sailing-card/sailing-image-wrapper.tsx
+++ components/cruisebound/sailing-card/sailing-image-wrapper.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {SailingDate} from "./sailing-date";
+import { SailingDate } from "./sailing-date";
 
 interface SailingImageWrapperProps {
   image: string;
@@ -14,11 +14,17 @@ export const SailingImageWrapper = ({
 }: SailingImageWrapperProps) => {
   return (
     <div className="relative overflow-hidden rounded-tl-sm rounded-bl-sm h-full w-full">
-      <Image className="w-full h-full object-cover object-center" src={image} alt={alt} width={200} height={150} />
+      <Image
+        className="w-full h-full object-cover object-center"
+        src={image || "https://placehold.co/500x600.png?text=No+Image"}
+        alt={alt}
+        width={200}
+        height={150}
+      />
 
       <div className="absolute top-4 left-2 bg-black/60 text-white p-1 text-sm rounded-sm">
         <SailingDate dates={date} />
       </div>
     </div>
   );
-}
+};
--- components/cruisebound/sailing-card/sailing-ship-info.tsx
+++ components/cruisebound/sailing-card/sailing-ship-info.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 export const SailingShipInfo = ({ logo, name }: CruiseLine) => {
   return (
-    <div className="flex flex-row md:flex-col md:items-end items-center space-x-2 md:">
+    <div className="flex flex-col md:items-end items-center space-x-2 gap-2 max-w-181">
       {" "}
       <Image
-        src={logo}
+        src={logo || 'https://placehold.co/105x80.png?text=No+Image'}
         alt={`${name} Logo`}
         width={80}
         height={60}
+        className="w-full"
       />
       <span className="text-sm text-muted-foreground font-medium text-gray-500">{name}</span>
     </div>
