0657e7b50f13697fc37d11ff4a9100833598977e
matoliva <olivamati@gmail.com>
2025-02-08 21:55:06 +1300

    feat: implement card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import { AppSidebar } from "@/components/ui/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { SidebarCustomTrigger } from "@/components/ui/sidebar-trigger";
 
+import {SailingCard} from "@/components/cruisebound/sailing-card/sailing-card";
+import { AppSidebar } from "@/components/ui/app-sidebar";
+
+import { useCruises } from "@/hooks/use-cruises";
+
 
 export default function Home() {
+  const { cruises, isLoading, isError } = useCruises();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <SidebarProvider defaultOpen>
       <AppSidebar />
-      <main className="min-h-screen">
+      <main className="min-h-screen w-full p-4">
         <SidebarCustomTrigger />
-        
+        <SailingCard cruise={cruises[0]} />
       </main>
     </SidebarProvider>
   );
