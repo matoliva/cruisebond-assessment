d64927de99b6082e9c4205129b4a3a650e138484
matoliva <olivamati@gmail.com>
2025-02-09 00:40:29 +1300

    fix: minor css fixes

--- components/cruisebound/sailing-card/sailing-footer.tsx
+++ components/cruisebound/sailing-card/sailing-footer.tsx
@@ -1,14 +1,11 @@
-
-
 interface SailingFooterProps {
-    children: React.ReactNode;
-  }
+  children: React.ReactNode;
+}
 
 export const SailingFooter = ({ children }: SailingFooterProps) => {
-    return (
-        <div className="flex items-center justify-between gap-4 border-t bg-gray-100 md:justify-end md:gap-8 p-4">
-            {children}
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-between gap-4 border-t md:justify-end md:gap-8 p-4">
+      {children}
+    </div>
+  );
 };
-
