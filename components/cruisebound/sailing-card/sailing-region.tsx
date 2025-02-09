5786840dd8d611efbf1e6f89b852721ac3a7ed08
matoliva <olivamati@gmail.com>
2025-02-09 16:23:00 +1300

    feat: implement use-filters hooks

--- components/cruisebound/sailing-card/sailing-region.tsx
+++ components/cruisebound/sailing-card/sailing-region.tsx
@@ -7,7 +7,7 @@ export const SailingRegion = ({ region, duration }: SailingRegionProps) => {
   return (
     <div className="flex flex-col md:flex-row items-center text-sm text-gray-500">
       <span className="mr-1">{`${region}`}</span>
-      <span className="ml-2">{`${duration} nights`}</span>
+      <span className="md:ml-2">{`${duration} nights`}</span>
     </div>
   );
 };
